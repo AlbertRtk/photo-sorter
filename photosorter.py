531083a064c03726f6d914239e6ebf78edfd7716
Albert <ratajczakalbert@gmail.com>
2018-06-08 22:23:37 +0200

    Adding function sortFiles. At the moment it prints tree and informs where are JPEG-files

--- photosorter.py
+++ photosorter.py
@@ -1,20 +1,57 @@
 from os import listdir
-from os.path import isdir, basename
-
+import os.path
+'''
 def printTree(folder, folderLevel):
-	print(folderLevel * "     " + basename(folder))
+	print(folderLevel * "     " + os.path.basename(folder))
 	content = listdir(folder)
-	if not content:
-		return
-	else:
+	if content:
 		folderLevel += 1
 		for piece in content:
 			piecePath = folder + '/' + piece
-			if isdir(piecePath):
+			if os.path.isdir(piecePath):
 				printTree(piecePath, folderLevel)
 			else:
-				print(folderLevel * "     " + piece)
+				print(folderLevel * "     " + os.path.split(piece)[1])
+	else:
+		return
+'''	
+		
+def sortFiles(folder, folderLevel):
+	print(folderLevel * "     " + os.path.basename(folder))
+	
+	content = listdir(folder)
+	
+	if content:
+		folderLevel += 1
+		
+		for piece in content:
+			piecePath = folder + '/' + piece
+			
+			if os.path.isdir(piecePath):
+				sortFiles(piecePath, folderLevel)
+			
+			elif os.path.isfile(piecePath):
+				print(folderLevel * "     " + os.path.split(piece)[1])
+				
+				if (os.path.splitext(piecePath)[1].lower() == '.jpg'
+				or os.path.splitext(piecePath)[1].lower() == '.jpeg'
+				or os.path.splitext(piecePath)[1].lower() == '.jpe'):
+					print('---> JPEG!!!')
+				
+				else:
+					print(os.path.splitext(piecePath)[1])
+			
+			else:
+				print(folderLevel * "     " + split(piece)[1])
+	
+	else:
+		return
+
+		
+#=======================================================================================================================
 
 inputPath = '../../photosorterTEST'
 
-printTree(inputPath, 0)
+#printTree(inputPath, 0)
+print('running...')
+sortFiles(inputPath, 0)
