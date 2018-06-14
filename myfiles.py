70fa9ddc16d5d00ec3bc727500438333d0ae7a3d
Albert <ratajczakalbert@gmail.com>
2018-06-14 21:03:35 +0200

    Adding function sortFiles (dirsTree + fortFile) and option of looking for duplicats

--- myfiles.py
+++ myfiles.py
@@ -2,7 +2,7 @@
 #=== class MyFiles - stors paths to input and output directories with files you want to sort============================
 #=======================================================================================================================
 #=== Albert Ratajczak ==================================================================================================
-#=== Last update: 2018-06-11 ===========================================================================================
+#=== Last update: 2018-06-14 ===========================================================================================
 #=======================================================================================================================
 
 import os
@@ -11,6 +11,9 @@ import shutil
 
 class MyFiles:
 
+	nFiles = int()
+	nDuplicats = int()
+	
 	def __init__(self, inputPath, outputPath):
 		self.inputPath = inputPath
 		self.outputPath = outputPath
@@ -22,7 +25,7 @@ class MyFiles:
 	def emptyFunc(self):
 		return
 	
-	
+#-----------------------------------------------------------------------------------------------------------------------	
 	
 	# methode dirsTree lists the tree of directories in inputPath
 	# if fullTree is True, files will be listed too
@@ -41,7 +44,7 @@ class MyFiles:
 			
 			# for each element in directory, create new MyFiles object
 			for piece in content:
-				piecePath = MyFiles(self.inputPath + '/' + piece, self.outputPath)
+				piecePath = MyFiles(os.path.join(self.inputPath, piece), self.outputPath)
 				
 				# if piecePath is directory, recursion of sorting methode sortFiles
 				if os.path.isdir(piecePath.inputPath):
@@ -58,35 +61,59 @@ class MyFiles:
 		else:
 			return
 	
-	
+#-----------------------------------------------------------------------------------------------------------------------
 	
 	# methode sortFile sorts (copies) file from inputPath to directory named by its modification date in outputPath
 	def sortFile(self):
+	
+		# get the file extension in lowercase
 		fileExt = os.path.splitext(self.inputPath)[1].lower()
+		fileName = os.path.basename(self.inputPath)
+		
 		sortExt = ['.jpg', '.jpeg', '.jpe']
 					
 		# if file is JPEG, set outpud directory name as file date of modification
 		# if file is not JPEG - output directory "other"
 		if any([fileExt == e for e in sortExt]):
 			modificationTime = os.path.getmtime(self.inputPath)
-			modificationTime = time.strftime('%Y-%m-%d', time.localtime(modificationTime))
-			outputDir = self.outputPath + '/' + modificationTime
+			outputDir = time.strftime('%Y-%m-%d', time.localtime(modificationTime))	
 		else:
-			outputDir = self.outputPath + '/other' 
+			outputDir = 'other' 
 		
+		# path to outputDir
+		outputDirPath = os.path.join(self.outputPath, outputDir)
+		dstName = os.path.join(outputDirPath, fileName)
+
+		if os.path.exists(dstName):
+			MyFiles.nDuplicats += 1
+			outputDir = 'duplicats'
+			outputDirPath = os.path.join(self.outputPath, outputDir)
+			fileName = '{:03}'.format(MyFiles.nDuplicats) + '_' + fileName
+			dstName = os.path.join(outputDirPath, fileName)
+			
 		# check if output directory exists and, if not, create it
-		if not os.path.exists(outputDir):
-			os.makedirs(outputDir)
+		if not os.path.exists(outputDirPath):
+			os.makedirs(outputDirPath)
 		
 		# copy the file to new directory
-		shutil.copy2(self.inputPath, outputDir)
+		shutil.copy2(self.inputPath, dstName)
+		MyFiles.nFiles += 1
 		print("{:>96}".format("copied to: " + outputDir))
 	
-
+#-----------------------------------------------------------------------------------------------------------------------	
+	
+	# methode sortFiles sorts files (copy) from inputPath and all sub-directories into outputPath
+	def sortFiles(self):
+		self.dirsTree(fullTree = True, func = MyFiles.sortFile)
+		print("{} files sorted in total, incuding {} duplicat(s)".format(MyFiles.nFiles, MyFiles.nDuplicats))
+		MyFiles.nFile = 0
+		MyFiles.nDuplicats = 0
+	
+#-----------------------------------------------------------------------------------------------------------------------
 	
 	# methode renameDirs renames all directories in inputPath directory (no other subdirectories are renamed)
 	# initial name hase to be in format: day.month.year
-	# new name will be: year-mont-day
+	# new name will be: year-month-day
 	def renameDirs(self):
 	
 		# print name of current directory
@@ -119,54 +146,4 @@ class MyFiles:
 		else:
 			print('The directory is empty.')
 	
-	
-	'''
-	# methode sortFiles sorts files (copy) from inputPath into outputPath
-	def sortFiles(self, dirLevel = 0):
-	
-		# print name of current directory with some space in fron to mark its level (dirLevel)
-		print(dirLevel * "\t" + os.path.basename(self.inputPath))
-	
-		# table with content of current directory
-		content = os.listdir(self.inputPath)
-	
-		# if directory is not empty
-		if content:
-			dirLevel += 1
-			
-			# for each element in directory, create new MyFiles object
-			for piece in content:
-				piecePath = MyFiles(self.inputPath + '/' + piece, self.outputPath)
-			
-				# if piecePath is directory, recursion of sorting methode sortFiles
-				if os.path.isdir(piecePath.inputPath):
-					piecePath.sortFiles(dirLevel)
-				
-				# if piecPath is file, get it extension and check if it is JPEG
-				else: #os.path.isfile(piecePath.inputPath):
-					fileExt = os.path.splitext(piecePath.inputPath)[1].lower()
-					
-					# if file is JPEG, set outpud directory name as file date of modification
-					# if file is not JPEG - output directory "other"
-					if ( fileExt == '.jpg' or fileExt == '.jpeg' or fileExt == '.jpe'):
-						modificationTime = os.path.getmtime(piecePath.inputPath)
-						modificationTime = time.strftime('%Y-%m-%d', time.localtime(modificationTime))
-						outputDir = piecePath.outputPath + '/' + modificationTime
-					else:
-						outputDir = piecePath.outputPath + '/other' 
-				
-					# print file name with extension and mark dirLevel with space
-					print(dirLevel * "\t" + os.path.split(piece)[1], end = "")
-					
-					# check if output directory exists and, if not, create it
-					if not os.path.exists(outputDir):
-						os.makedirs(outputDir)
-				
-					# copy the file to new directory
-					shutil.copy2(piecePath.inputPath, outputDir)
-					print(" --> copied to: " + outputDir)
-		
-		# if current directory is empty - return
-		else:
-			return
-	'''
+#=== end of file =======================================================================================================	
--- photosorter.py
+++ photosorter.py
@@ -2,38 +2,20 @@
 #=== photosorter - order and sort your photos ==========================================================================
 #=======================================================================================================================
 #=== Albert Ratajczak ==================================================================================================
-#=== Last update: 2018-06-09 ===========================================================================================
+#=== Last update: 2018-06-14 ===========================================================================================
 #=======================================================================================================================
 #=======================================================================================================================
 
 from myfiles import *
 
-'''
-def printTree(folder, folderLevel):
-	print(folderLevel * "     " + os.path.basename(folder))
-	content = listdir(folder)
-	if content:
-		folderLevel += 1
-		for piece in content:
-			piecePath = folder + '/' + piece
-			if os.path.isdir(piecePath):
-				printTree(piecePath, folderLevel)
-			else:
-				print(folderLevel * "     " + os.path.split(piece)[1])
-	else:
-		return
-'''
-		
-#=======================================================================================================================
-
 inputPath = 'F:/photosorterTEST'
 outputPath = 'sorted'
 
-print('running...')
-
 myPhotos = MyFiles(inputPath, outputPath)
 
-myPhotos.dirsTree(fullTree = True)#, func = MyFiles.sortFile)
+myPhotos.dirsTree(fullTree = True)
 
-#myPhotos.sortFiles()
+myPhotos.sortFiles()
+
+
 
